d83572686273e6e7eb6064864bebab50779fe0a1
networkelements <bleuscr.nix@gmail.com>
2016-08-11 20:44:45 +0000

    Update configuration.nix
    https://nixos.org/wiki/Lenovo_Thinkpad_x200
    https://nixos.org/wiki/Lenovo_Thinkpad_x220
    https://gist.github.com/rehno-lindeque/4eb5a325202c840cb8c9
    https://nixos.org/wiki/Encrypted_Root_on_NixOS
    https://nixos.org/wiki/Input_Method_Editor
    http://gdgdblg.blogspot.jp/2014/04/tp-smapi.html
    https://wiki.archlinuxjp.org/index.php/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%81%A8%E3%82%B0%E3%83%AB%E3%83%BC%E3%83%97

--- X220/configuration.nix
+++ X220/configuration.nix
@@ -1,4 +1,164 @@
-  boot.initrd.luks.devices = [
-    { name = "luksroot"; device = "/dev/sda2"; }
-  #  { name = "swap"; device = "/dev/sda3"; }
-  ];
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+  
+  boot = 
+  {
+    initrd = 
+    { 
+      luks = 
+      { 
+        devices = 
+          [
+            { name = "luksroot";
+              device = "/dev/sda2"; 
+              allowDiscards = true;
+            }
+            #  { name = "swap"; device = "/dev/sda3"; }
+          ];
+        
+        cryptoModules = 
+          [ 
+            "aes" 
+            "sha256" 
+            "sha1" 
+            "cbc" 
+          ];
+      };
+
+  
+      kernelModules = 
+        [ 
+          "usb_storage" 
+          "dm_snapshot" 
+          "fbcon" 
+          "kvm-intel"
+          "tp_smapi"
+        ];
+      
+      availableKernelModules = 
+        [ 
+          "ehci_pci" 
+          "ahci" 
+          "usb_storage" 
+        ];
+        
+      extraModulePackages = 
+        [
+          config.boot.kernelPackages.tp_smapi
+        ];
+    };
+    
+    loader  = 
+      {
+        grub = 
+          {
+            enable = true;
+            version = 2;
+            device = "/dev/sda1";
+          };
+        
+        gummiboot.enable = true;
+        #efi.canTouchEfiVariables = true;
+      };
+  };
+  
+    networking = 
+    {
+      hostName =  "destroyer";
+      wireless.enable = true;
+      
+    };
+
+   i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+     consoleKeyMap = "jp106";
+     defaultLocale = "ja.JP_UTF-8";
+   };
+
+   time.timeZone = "Asia/Tokyo";
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+   environment.systemPackages = with pkgs; 
+   [
+      wget
+      (fcitx-with-plugins.override { plugins = [ anthy ]; })
+      fcitx-configtool
+      f2fs-tools
+      fish
+      cryptsetup
+      firefox
+    ];
+
+  services = 
+  {
+    openssh.enable = true;
+    #printing.enable = true;  #CUPS printing
+
+    xserver = 
+    {
+      enable = true;
+      layout = "jp";
+      #xkblayout = "jp";
+      xkbOptions = "japan";
+
+      #displayManager 
+      #{
+        #kdm.enable = true;
+        #kde4.enable = true;
+      #};
+
+      desktopManager = 
+      {
+        gnome3.enable = true;
+        default = "gnome3";
+      };
+      
+      #windowManager = 
+        #{
+          #xmonad.enable = true; 
+          #xmonad.enableContribAndExtras = true;
+        #};
+    };
+  };
+
+  # > which fish
+  ## change path fish shell
+  # > sudo useradd -m -g users -G wheel -s /usr/bin/fish muarsame ; passwd murasame
+  users.extraUsers.username = 
+  {
+    createHome = true;
+    home = "/home/murasame";
+    description = "yappari kuchikukan ha saikou daze!";
+    extraGroups = [ ];
+    isSystemUser = true;
+    useDefaultShell = true;
+    
+  };
+
+
+
+
+
+
+
+
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.extraUsers.guest = {
+  #   isNormalUser = true;
+  #   uid = 1000;
+  # };
+
+  # The NixOS release to be compatible with for stateful data such as databases.
+  system.stateVersion = "16.03";
+}
