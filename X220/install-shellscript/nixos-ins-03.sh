432bf24814b9559041aac21b34c040b3bbdd9bbf
networkelements <bleuscr.nix@gmail.com>
2017-03-18 18:15:03 +0900

    Create nixos-ins-03.sh

---
+++ X220/install-shellscript/nixos-ins-03.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+pvcreate /dev/mapper/enc-pv
+vgcreate vg /dev/mapper/enc-pv
+lvcreate -l '100%FREE' -n root vg
+mkfs.fat -F32 /dev/sda1
+
+mount /dev/vg/root /mnt
+mkdir /mnt/boot
+mount /dev/mapper/cryptboot /mnt/boot
+mkdir /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+lsblk
+
+nixos-generate-config --root /mnt
+cd /mnt/etc/nixos
+mv configuration.nix _old01_configuration.nix
+mv hardware-configuration.nix _old01_hardware-configuration.nix
+# curl https://raw.githubusercontent.com/networkelements/NixOS/master/X220/configuration.nix -o /mnt/etc/nixos/configuration.nix
+# curl https://raw.githubusercontent.com/networkelements/NixOS/master/X220/hardware-configuration.nix -o /mnt/etc/nixos/hardware-configuration.nix
+# grep "device" _old01_hardware-configuration.nix >> hardware-configuration.nix
+emacs -nw configuration.nix
