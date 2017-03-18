142d4338114a5758bc1f934cdeb49bf5e1450f92
networkelements <bleuscr.nix@gmail.com>
2017-03-18 18:32:12 +0900

    Update nixos-ins-01.sh

--- X220/install-shellscript/nixos-ins-01.sh
+++ X220/install-shellscript/nixos-ins-01.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 loadkeys jp106
 
-https://raw.githubusercontent.com/networkelements/NixOS/master/X220/install-shellscript/nixos-ins-02.sh -o nixos-ins-02.sh
+curl https://raw.githubusercontent.com/networkelements/NixOS/master/X220/install-shellscript/nixos-ins-02.sh -o nixos-ins-02.sh
+
 chmod +x nixos-ins-02.sh
-https://raw.githubusercontent.com/networkelements/NixOS/master/X220/install-shellscript/nixos-ins-03.sh- o nixos-ins-03.sh
+
+curl https://raw.githubusercontent.com/networkelements/NixOS/master/X220/install-shellscript/nixos-ins-03.sh -o nixos-ins-03.sh
+
 chmod +x nixos-ins-03.sh
 
 script `date '+%Y%m%d-%H:%M:%S'`.log
 
 
-sh nixos-ins-02.sh
+#sh nixos-ins-02.sh
 
